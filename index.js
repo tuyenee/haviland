62b67a3d43d31ec1829044d739b981b90b719cfc
Tuyen Thuc Nguyen <tutnguyen@Tuyens-MacBook-Pro.local>
2018-11-01 14:45:54 +0700

    Implemented user/password login for Mongo, credentials are stored in env file

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const _PORT = 8080;
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const mongoDb = 'mongodb://mongo:27017/haviland';
 const passport = require('passport');
 const bodyParser = require('body-parser');
 const User = require('./models/user');
@@ -13,8 +12,14 @@ const session = require('express-session');
 app.listen(_PORT, () => console.log("Server is running on " + _PORT + ', will be mapped to 8087' ));
 
 /* Mongo connection */
-mongoose.connect(mongoDb, {useNewUrlParser: true});
-//mongoose.Promise = global.Promise;
+const mongoDb = 'mongodb://mongo:27017';
+console.log('READ from the ENV:  ', process.env.MONGO_INITDB_DATABASE);
+mongoose.connect(mongoDb, {
+    authSource: process.env.MONGO_INITDB_AUTH_DATABASE,
+    auth: {user: process.env.MONGO_INITDB_ROOT_USERNAME, password: process.env.MONGO_INITDB_ROOT_PASSWORD},
+    dbName: process.env.MONGO_INITDB_DATABASE,
+    useNewUrlParser: true
+});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Mongo connection error:'));
 
