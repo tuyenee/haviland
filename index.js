af0fa8e9acaacbaad8ffeb1048d90db748d9b105
Tuyen Thuc Nguyen <tutnguyen@Tuyens-MacBook-Pro.local>
2018-11-02 16:09:28 +0700

    Moved passport to middlewares, cleaned up the index file

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ const bodyParser = require('body-parser');
 const ejsLayouts = require('express-ejs-layouts');
 const flash = require('express-flash-messages');
 const session = require('express-session');
-const myPassport = require('./middlewares/myPassport');
-var passport = require('passport')
-  , LocalStrategy = require('passport-local').Strategy;
 
 
 app.listen(_PORT, () => console.log("Server is running on " + _PORT + ', will be mapped to 8087' ));
@@ -46,6 +43,13 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(ejsLayouts);
 
+
+/* Passport */
+const passport = require('passport');
+const myPassport = require('./middlewares/myPassport');
+myPassport(app, passport);
+
+
 /* Routing */
 const genericRoute = require('./routes/generic');
 const usersRoute = require('./routes/users');
@@ -55,39 +59,4 @@ app.use('/users', usersRoute);
 app.get('/', (req, res) => {
         res.render('index');
     }
-);
-
-/* Passport */
-const User = require('./models/user');
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.serializeUser((user, callback) => callback(null, user.id));
-passport.deserializeUser((id, callback) => User.findById(id, (err, user) => callback(err, user)));
-
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    User.findOne({ username: username }, function (err, user) {
-        if (err) { 
-            return done(err); 
-        }
-        if (!user) {
-            return done(null, false, { message: 'Incorrect username.' });
-        }
-        if (!user.validPassword(password)) {
-            return done(null, false, { message: 'Incorrect password.' , type: 'danger' });
-        }
-        return done(null, user);
-    });
-  }
-));
-
-
-app.post('/login',
-    passport.authenticate('local', { 
-        successRedirect: '/',
-        failureRedirect: '/login',
-        failureFlash: true
-    })
 );
--- middlewares/myPassport.js
+++ middlewares/myPassport.js
@@ -1,43 +1,36 @@
-// const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
+let LocalStrategy = require('passport-local').Strategy;
 const User = require('../models/user');
 
 module.exports = function(app, passport) {
+
     app.use(passport.initialize());
     app.use(passport.session());
 
-    app.get('/success', (req, res) => res.send('Logged in successfully'));
-    app.get('/error', (req, res) => res.send('Wrong credentialsssss'));
-
-    console.log('My Passport got called');
     passport.serializeUser((user, callback) => callback(null, user.id));
-
     passport.deserializeUser((id, callback) => User.findById(id, (err, user) => callback(err, user)));
 
     passport.use(new LocalStrategy(
         function(username, password, done) {
-            console.log('Attemp to login: ', username, password);
-            User.findOne({
-              username: username
-            }, function(err, user) {
-                console.log('Found the following user in database: ', user.username, user.password);
-                if (err) {
-                    return done(err);
+            User.findOne({ username: username }, function (err, user) {
+                if (err) { 
+                    return done(err); 
                 }
-
                 if (!user) {
-                    return done(null, false);
+                    return done(null, false, { message: 'Incorrect username.', type: 'danger' });
                 }
-
-                if (user.password != password) {
-                    return done(null, false);
+                if (!user.validPassword(password)) {
+                    return done(null, false, { message: 'Incorrect password.' , type: 'danger' });
                 }
                 return done(null, user);
             });
         }
     ));
+        
     app.post('/login',
-        passport.authenticate('local', { failureRedirect: '/error' }),
-        (req, res) => res.redirect('/success')
+        passport.authenticate('local', { 
+            successRedirect: '/',
+            failureRedirect: '/login',
+            failureFlash: true
+        })
     );
 }
