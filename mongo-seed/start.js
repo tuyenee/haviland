1a93669b678c7c97a5a75256b66d827f75dd2397
Tuyen Thuc Nguyen <tutnguyen@tutnguyen-mac-2018.local>
2018-11-07 11:23:57 +0700

    Seed rooms on startup

--- mongo-seed/start.js
+++ mongo-seed/start.js
@@ -1,6 +1,7 @@
 db.haviland.drop();
 db.users.insertMany([
     {
+        "_id": "5be2570e76e440ab73cd81a6",
         "name": "John Doe",
         "username": "jsmith",
         "password": "fcccb02b53f4bcba0c5ec3bc209f6c6a629b44cfb38ec91cbfc3078214398bfcfaac154442cc5eab0d0ff226f7bfbd19ae7f82ac26ef9f056ffc5fb32b994ad8",
@@ -10,6 +11,7 @@ db.users.insertMany([
         "admin": true
     },
     {
+        "_id": "5be2570e76e440ab73cd81a7",
         "name": "Jen Deer",
         "username": "jford",
         "password": "61f0d30fd44f23cef1309205ec4392424eb5760225bafe4bb7166f9efe2bba9b094b03acc9c9c5df17a0c466e80628e2a1941f7b73a273c0b858dfbd955942fb",
@@ -31,7 +33,7 @@ db.rooms.insertMany([
         "building": "DTH",
         "address": "65/5 Dinh Tien Hoang, Hai Chau, Da Nang",
         "number": 202,
-        "occupant": "",
+        "occupant": "5be2570e76e440ab73cd81a7",
         "price": 3000000
     },
     {
