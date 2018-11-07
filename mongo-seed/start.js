691f33b6a66e8291b5c71ed01ec7625981984b8c
Tuyen Thuc Nguyen <tutnguyen@tutnguyen-mac-2018.local>
2018-11-07 11:42:53 +0700

    Fixed bug while seeding users - id field with ObjectId instead of plain text id

--- mongo-seed/start.js
+++ mongo-seed/start.js
@@ -1,7 +1,7 @@
 db.haviland.drop();
 db.users.insertMany([
     {
-        "_id": "5be2570e76e440ab73cd81a6",
+        "_id": (new ObjectId("5be2570e76e440ab73cd81a6")),
         "name": "John Doe",
         "username": "jsmith",
         "password": "fcccb02b53f4bcba0c5ec3bc209f6c6a629b44cfb38ec91cbfc3078214398bfcfaac154442cc5eab0d0ff226f7bfbd19ae7f82ac26ef9f056ffc5fb32b994ad8",
@@ -11,7 +11,7 @@ db.users.insertMany([
         "admin": true
     },
     {
-        "_id": "5be2570e76e440ab73cd81a7",
+        "_id": (new ObjectId("5be2570e76e440ab73cd81a7")),
         "name": "Jen Deer",
         "username": "jford",
         "password": "61f0d30fd44f23cef1309205ec4392424eb5760225bafe4bb7166f9efe2bba9b094b03acc9c9c5df17a0c466e80628e2a1941f7b73a273c0b858dfbd955942fb",
