1497405b77d1b352653ac194111791225d106c77
Tuyen Thuc Nguyen <tutnguyen@tutnguyen-mac-2018.local>
2018-11-23 13:40:17 +0700

    Implemented Google ReCaptcha for login page

--- middlewares/myCaptcha.js
+++ middlewares/myCaptcha.js
@@ -1,7 +1,31 @@
-module.exports = function(req, res, done) {
-    console.log('Im checking the captcha');
-    if(1 || captchaIsGood) {
+const request = require('request');
+const verificationEndpoint = 'www.google.com/recaptcha/api/siteverify';
+const mySecretKey = '6LcUW3wUAAAAACdwNrj4a-YlS3BcHQ6NJRPSEEv5';
 
-    }
-    return done()
+const validateCaptcha = function(captchaResponse) {
+    return new Promise(function(resolve, reject) {
+        const url = `https://${verificationEndpoint}?secret=${mySecretKey}&response=${captchaResponse}`;
+        request(url, function(error, response, body) {
+            body = JSON.parse(body);
+            // Conditions here
+            if(error) {
+                reject('Could not verify captcha, please try again');
+            }
+            if(body.success) {
+                resolve(true);
+            } else {
+                reject('Please solve the provided captcha');
+            }
+        })
+    });
+}
+
+module.exports = function(req, res, done) {
+    const receivedCaptcha = req.body['g-recaptcha-response'] || 'test';
+    validateCaptcha(receivedCaptcha).then(function(success) {
+        done();
+    }, function(message) {
+        req.flash('danger', message);
+        res.redirect('/login');
+    });
 }
